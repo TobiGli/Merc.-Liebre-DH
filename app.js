bea296fb83228bf02c36a533fe2f58100c52b4e6
TobiGLi <tobiasgomez99.tg@gmail.com>
2024-02-21 20:51:17 -0300

    Arreglado bug que no se veia las imgs, y comienzo de la parte 2

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 });
 
+app.use(express.static('public'));
+
 app.listen(8000, () => 
 console.log('Levantando un servidor con Express'));
