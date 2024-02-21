1f4e91f38d09e3855ffa200c769b564da532284d
TobiGLi <tobiasgomez99.tg@gmail.com>
2024-02-21 19:12:26 -0300

    Realizado header

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require ('path');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+});
+
+app.listen(8000, () => 
+console.log('Levantando un servidor con Express'));
